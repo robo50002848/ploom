ec0309d0dcee4c0c3857a9eb5c41bfe7f5fa87b1
Robert Frańczak <robertfranczak@MacBook-Pro-Robert.local>
2025-03-10 13:34:21 +0100

    change timeouts and remove viewport for testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,15 +2,15 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
     testDir: './tests',
-    timeout: process.env.CI ? 120000 : 30000,
+    timeout: 120000,
     expect: {
-        timeout: process.env.CI ? 60000 : 30000,
+        timeout: 120000,
     },
     retries: process.env.CI ? 1 : 0,
     workers: process.env.CI? 1 : 6,
     use: {
         headless: process.env.CI ? true : false,
-        viewport: process.env.CI ? { width: 1920, height: 1080 } : null,
+        // viewport: process.env.CI ? { width: 1920, height: 1080 } : null,
         launchOptions: {
             args: ['--start-maximized'],
         },
