4b2f48c7ba2dbb8c8757a8fff491d34cc5b16f64
Robert Frańczak <robertfranczak@MacBook-Pro-Robert.local>
2025-03-10 13:27:15 +0100

    increase general test timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
     testDir: './tests',
-    timeout: process.env.CI ? 60000 : 30000,
+    timeout: process.env.CI ? 120000 : 30000,
     expect: {
         timeout: process.env.CI ? 60000 : 30000,
     },
